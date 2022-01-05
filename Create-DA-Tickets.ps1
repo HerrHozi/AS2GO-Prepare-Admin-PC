6a856c3518f88332a5f4eac02dc76524937b9d2a
HerrHozi <96825160+HerrHozi@users.noreply.github.com>
2022-01-05 13:39:40 +0100

    Add files via upload

---
+++ Create-DA-Tickets.ps1
@@ -0,0 +1,31 @@
+﻿# Created by hozimmer@microsoft.com
+# create a short cut on C:\Users\Public\Desktop
+# start with Domain Admin (DA-xxxxx) on ADMIN Workstation for command 'dir \\<your DC>\c$' 
+
+
+$User = $env:USERNAME
+$DC = $env:LOGONSERVER
+$path = "$DC\c$"
+$sleepintervalsec = 300
+$sum = 0
+$sum -= $sleepintervalsec
+
+
+while ($true)
+{
+    $sum += $sleepintervalsec
+    $min = $sum/60
+    $hours = $sum/3600
+    Invoke-Expression "dir $path"
+    Write-Host "`n`n... script runs since $sum seconds or $min minutes or $hours hours on $env:COMPUTERNAME" -ForegroundColor Green
+
+
+   foreach ($step in (1..$sleepintervalsec)) 
+   {
+      write-progress -Activity "Next command - dir $path as user $User from $env:COMPUTERNAME" -Status "Waiting - Press any key to stop" -SecondsRemaining ($sleepintervalsec-$step) -PercentComplete  ($step/$sleepintervalsec*100)
+      start-sleep -seconds 1
+   }
+
+    clear
+
+}
---
+++ Create-Shortcut.ps1
@@ -0,0 +1,6 @@
+﻿$SourceFilePath = "C:\Temp\AS2Go\Create-DA-Tickets.ps1"
+$ShortcutPath = "C:\Users\Public\Desktop\Create Tickets for PtT.lnk"
+$WScriptObj = New-Object -ComObject ("WScript.Shell")
+$shortcut = $WscriptObj.CreateShortcut($ShortcutPath)
+$shortcut.TargetPath = $SourceFilePath
+$shortcut.Save()
